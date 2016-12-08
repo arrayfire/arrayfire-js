f718aa7a69616ad218014d4b2a901349daf9c2ab
unbornchikken <gabor.mezo@outlook.com>
2016-12-08 22:43:04 +0100

    AFArray in progress

--- test/es6/unified.js
+++ test/es6/unified.js
@@ -21,24 +21,24 @@ describe('unified backend functions', function () {
     });
 
     it('should give available backends', function () {
-        assert(_.isObject(af.backends));
+        assert(_.isObject(af.backend));
         assert(_.isFunction(af.getAvailableBackends));
 
-        const cpu = af.getAvailableBackends() & af.backends.CPU;
-        const cuda = af.getAvailableBackends() & af.backends.CUDA;
-        const ocl = af.getAvailableBackends() & af.backends.OPENCL;
+        const cpu = af.getAvailableBackends() & af.backend.CPU;
+        const cuda = af.getAvailableBackends() & af.backend.CUDA;
+        const ocl = af.getAvailableBackends() & af.backend.OPENCL;
 
         assert(cpu && (cuda || ocl));
         assert.equal(af.getBackendCount(), (cpu && 1) + (cuda && 1) + (ocl && 1));
     });
 
     it('should switch to other backends', function () {
-        const cuda = af.getAvailableBackends() & af.backends.CUDA;
-        const ocl = af.getAvailableBackends() & af.backends.OPENCL;
+        const cuda = af.getAvailableBackends() & af.backend.CUDA;
+        const ocl = af.getAvailableBackends() & af.backend.OPENCL;
 
-        cuda && tryBackend(af.backends.CUDA);
-        ocl && tryBackend(af.backends.OPENCL);
-        tryBackend(af.backends.CPU);
+        cuda && tryBackend(af.backend.CUDA);
+        ocl && tryBackend(af.backend.OPENCL);
+        tryBackend(af.backend.CPU);
 
         function tryBackend(backend) {
             af.setBackend(backend);
